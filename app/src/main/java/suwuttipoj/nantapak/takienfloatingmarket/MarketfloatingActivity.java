7c7dc2b7b3a10a091b435b1e612f180d569f2182
masterUNG <phrombutr@gmail.com>
2016-11-27 14:51:58 +0700

    ListView OK

--- app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/MarketfloatingActivity.java
+++ app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/MarketfloatingActivity.java
@@ -7,6 +7,9 @@ import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.widget.ListView;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 
 public class MarketfloatingActivity extends AppCompatActivity {
 
@@ -28,7 +31,25 @@ public class MarketfloatingActivity extends AppCompatActivity {
 
             Log.d("27novV1", "JSON ==> " + strJSON);
 
+            JSONArray jsonArray = new JSONArray(strJSON);
 
+            String[] nameStrings = new String[jsonArray.length()];
+            String[] detailStrings = new String[jsonArray.length()];
+            String[] iconStrings = new String[jsonArray.length()];
+
+            for (int i=0;i<jsonArray.length();i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+
+                nameStrings[i] = jsonObject.getString("SHOP_NAME");
+                detailStrings[i] = jsonObject.getString("SHOP_DETAIL");
+                iconStrings[i] = jsonObject.getString("SHOP_IMAGE_FILE");
+
+            }   // for
+
+            ShopAdapter shopAdapter = new ShopAdapter(MarketfloatingActivity.this,
+                    iconStrings, nameStrings, detailStrings);
+            listView.setAdapter(shopAdapter);
 
         } catch (Exception e) {
             e.printStackTrace();
