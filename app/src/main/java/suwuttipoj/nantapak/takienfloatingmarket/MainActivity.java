52c19beb56a349b6b5a49c5434fef630745d0ad6
nantapak <nantapak1011@gmail.com>
2016-12-18 13:52:40 +0700

    Start 18 Dec

--- app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/MainActivity.java
+++ app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/MainActivity.java
@@ -7,7 +7,7 @@ import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
-    // นี่คือการ update โดย มาสเตอร์ อึ่ง
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
