cc39c8375365e314ce5106590621b1f003c2e33e
masterUNG <phrombutr@gmail.com>
2016-12-23 15:34:32 +0700

    Spinner Show OK

--- app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/EditUser.java
+++ app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/EditUser.java
@@ -3,30 +3,82 @@ package suwuttipoj.nantapak.takienfloatingmarket;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 public class EditUser extends AppCompatActivity {
 
     //Explicit
-    private String userIDString, tab = "23decV3";
-    private MySynchronize mySynchronize;
+    private String  tab = "23decV3", tag1 = "23decV4", tag2 = "23decV5";
+    private String userIDString, PREFIX_ID, showPREFIX;
+    private TextView prefixTextView;
+    private Spinner prefixSpinner;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_edit_user);
 
+        //Bind Widget
+        bindWidget();
+
         //Setup
         userIDString = getIntent().getStringExtra("USER_ID");
         Log.d(tab, "USER_ID ==> " + userIDString);
-        mySynchronize = new MySynchronize(EditUser.this);
 
         //Get Value From menID
         getValueFromMemID();
 
+        //Create Prefix Spinner
+        createPrefixSpinner();
 
 
 
     }   // Main Method
+
+    private void createPrefixSpinner() {
+
+        String urlJSON = "http://swiftcodingthai.com/ton/get_prefix.php";
+
+        try {
+
+            MySynAll mySynAll = new MySynAll(EditUser.this);
+            mySynAll.execute(urlJSON);
+            String s = mySynAll.get();
+            Log.d(tag2, "JSON ==> " + s);
+
+            JSONArray jsonArray = new JSONArray(s);
+            String[] prefixStrings = new String[jsonArray.length()];
+            for (int i=0;i<jsonArray.length();i++) {
+
+                JSONObject jsonObject = jsonArray.getJSONObject(i);
+                prefixStrings[i] = jsonObject.getString("PREFIX_NAME");
+
+            }   // for
+
+            ArrayAdapter<String> stringArrayAdapter = new ArrayAdapter<String>(EditUser.this,
+                    android.R.layout.simple_list_item_1, prefixStrings);
+            prefixSpinner.setAdapter(stringArrayAdapter);
+
+
+        } catch (Exception e) {
+            Log.d(tag2, "e createSpinner ==> " + e.toString());
+        }
+
+    }
+
+    private void bindWidget() {
+
+        prefixTextView = (TextView) findViewById(R.id.textView24);
+        prefixSpinner = (Spinner) findViewById(R.id.spinner4);
+
+    }
 
     private void getValueFromMemID() {
 
@@ -34,14 +86,49 @@ public class EditUser extends AppCompatActivity {
 
         try {
 
+            MySynchronize mySynchronize = new MySynchronize(EditUser.this);
             mySynchronize.execute("MEM_ID", userIDString, urlJSON);
             String s = mySynchronize.get();
             Log.d(tab, "JSON of getValueFromMenID ==> " + s);
 
+            JSONArray jsonArray = new JSONArray(s);
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+
+            PREFIX_ID = jsonObject.getString("PREFIX_ID");
+            Log.d(tag1, "PREFIX_ID ==> " + PREFIX_ID);
+            showPREFIX = findPREFIX(PREFIX_ID);
+            prefixTextView.setText(showPREFIX);
+
+
+            mySynchronize.cancel(true);
         } catch (Exception e) {
             Log.d(tab, "e getValue ==> " + e.toString());
         }
 
     }   // getValue
+
+    private String findPREFIX(String prefix_id) {
+
+        String result = null;
+        String urlJSON = "http://swiftcodingthai.com/ton/get_prefix_where.php";
+
+        try {
+
+            MySynchronize mySynchronize = new MySynchronize(EditUser.this);
+            mySynchronize.execute("PREFIX_ID", prefix_id, urlJSON);
+            String s = mySynchronize.get();
+            Log.d(tag1, "JSON ==> " + s);
+
+            JSONArray jsonArray = new JSONArray(s);
+            JSONObject jsonObject = jsonArray.getJSONObject(0);
+            result = jsonObject.getString("PREFIX_NAME");
+
+
+        } catch (Exception e) {
+            Log.d(tag1, "e findPREFIX ==> " + e.toString());
+        }
+
+        return result;
+    }
 
 }   // Main Class
---
+++ app/src/main/java/suwuttipoj/nantapak/takienfloatingmarket/MySynAll.java
@@ -0,0 +1,41 @@
+package suwuttipoj.nantapak.takienfloatingmarket;
+
+import android.content.Context;
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.squareup.okhttp.OkHttpClient;
+import com.squareup.okhttp.Request;
+import com.squareup.okhttp.Response;
+
+/**
+ * Created by masterUNG on 12/23/2016 AD.
+ */
+
+public class MySynAll extends AsyncTask<String, Void, String>{
+
+    private Context context;
+
+    public MySynAll(Context context) {
+        this.context = context;
+    }
+
+    @Override
+    protected String doInBackground(String... strings) {
+
+        try {
+
+            OkHttpClient okHttpClient = new OkHttpClient();
+            Request.Builder builder = new Request.Builder();
+            Request request = builder.url(strings[0]).build();
+            Response response = okHttpClient.newCall(request).execute();
+            return response.body().string();
+
+        } catch (Exception e) {
+            Log.d("23devV5", "e doin ==> " + e.toString());
+            return null;
+        }
+
+
+    }
+}   // Main Class
